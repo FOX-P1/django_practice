85e1a7d4212caf450771ad92900f55f0b49cc070
FOX-P1 <smgms7513@gmail.com>
2021-08-26 16:31:56 +0900

    django_practice_1

---
+++ config/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin∑
+from django.urls import path, include
+from pybo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('pybo/', include('pybo.urls')),
+]
